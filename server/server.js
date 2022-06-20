1b34379080f45010f47e6f250d5536e8e52a6e2c
Erica <erica.trenholm48@gmail.com>
2022-06-19 20:18:13 -0400

    deploying

--- server/server.js
+++ server/server.js
@@ -22,12 +22,12 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app })
 
   // if we're in production, serve client/build as static assets
-  // if (process.env.NODE_ENV === 'production') {
-  //   app.use(express.static(path.join(__dirname, '../client/build')));
-  // }
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '../client/build/index.html'))
-  // })
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
 
   db.once('open', () => {
     app.listen(PORT, () => {
